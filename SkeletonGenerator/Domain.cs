4fa943470c9d294d30c6e0e7724a186ca1f08f47
Rubens Cordeiro <zyoncore@hotmail.com>
2023-07-23 20:47:02 -0300

    updated Domain

--- SkeletonGenerator/Domain.cs
+++ SkeletonGenerator/Domain.cs
@@ -1,4 +1,12 @@
-﻿public class Source
+﻿namespace SkeletonGenerator.Domain;
+
+public class SourcesModel
+{
+    public SourceModel? SourceTs { get; init; }
+    public SourceModel? SourceCSharp { get; init; }
+}
+ 
+public class SourceModel
 {
     public required string Path { get; set; }
     public required List<ClassModel> Classes { get; init; }
@@ -18,6 +26,22 @@ public class ConstructorModel
 {
     public required string Name { get; init; }
     public required List<ParameterModel> Parameters { get; init; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is ConstructorModel other)
+        {
+            return other.Name == Name &&
+                   other.Parameters.Count == Parameters.Count &&
+                   other.Parameters.All(x => Parameters.Contains(x));
+        }
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode() ^ Parameters.GetHashCode();
+    }
 }
 
 
@@ -26,6 +50,20 @@ public class PropertyModel
     public required string Name { get; init; }
     public required string Type { get; init; }
     public bool IsStatic { get; init; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is PropertyModel other)
+        {
+            return other.Name == Name &&
+                    other.Type == Type;
+        }
+        return base.Equals(obj);
+    }
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode() ^ Type.GetHashCode();
+    }
 }
 
 public class MethodModel
@@ -34,18 +72,64 @@ public class MethodModel
     public required List<ParameterModel> Parameters { get; init; }
     public required string ReturnType { get; init; }
     public bool IsStatic { get; init; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is MethodModel other)
+        {
+            return other.Name == Name &&
+                    other.ReturnType == ReturnType &&
+                    other.Parameters.Count == Parameters.Count &&
+                    other.Parameters.All(x => Parameters.Contains(x));
+        }
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode() ^ ReturnType.GetHashCode() ^ Parameters.GetHashCode();
+    }
 }
 
 public class ParameterModel
 {
     public required string Name { get; init; }
     public required string Type { get; init; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is ParameterModel other)
+        {
+            return other.Name == Name &&
+                   other.Type == Type;
+        }
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode() ^ Type.GetHashCode();
+    }
 }
 
 public class EnumModel
 {
     public required string Name { get; init; }
     public required List<EnumValueModel> Values { get; init; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is EnumModel other)
+        {
+            return other.Name == Name;
+        }
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode();
+    }
 }
 
 public class InterfaceModel
@@ -53,10 +137,38 @@ public class InterfaceModel
     public required string Name { get; init; }
     public required List<PropertyModel> Properties { get; init; }
     public required List<MethodModel> Methods { get; init; }
+
+    public override bool Equals(object? obj)
+    {
+        if(obj is InterfaceModel model)
+        {
+            return this.Name == model.Name;
+        }
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode();
+    }
 }
 
 public class EnumValueModel
 {
     public required string Name { get; init; }
     public required string Value { get; init; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is EnumValueModel other)
+        {
+            return other.Name == Name &&
+                   other.Value == Value;
+        }
+        return base.Equals(obj);
+    }
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode() ^ Value.GetHashCode();
+    }
 }
