da9e4254e1de55a841b40785aff00680e93107fe
Rubens Cordeiro <zyoncore@hotmail.com>
2023-08-05 15:15:03 -0300

    updated Domain

--- SkeletonGenerator/Domain.cs
+++ SkeletonGenerator/Domain.cs
@@ -1,30 +1,114 @@
 ﻿namespace SkeletonGenerator.Domain;
-
-public class SourcesModel
+internal class SourcesModel
 {
-    public SourceModel? SourceTs { get; init; }
-    public SourceModel? SourceCSharp { get; init; }
+    public SourceModel? SourceTs { get; set; }
+    public SourceModel? SourceCSharp { get; set; }
+    public List<TypeModel> SourceTypes
+    {
+        get
+        {
+            var types = new List<TypeModel>();
+            if (SourceTs is not null)
+            {
+                types.AddRange(SourceTs.SourceTypes);
+            }
+            if (SourceCSharp is not null)
+            {
+                types.AddRange(SourceCSharp.SourceTypes);
+            }
+            return types;
+        }
+    }
 }
- 
 public class SourceModel
 {
     public required string Path { get; set; }
     public required List<ClassModel> Classes { get; init; }
     public required List<EnumModel> Enums { get; init; }
     public required List<InterfaceModel> Interfaces { get; init; }
+
+    public TypeModel[] SourceTypes => Classes.Cast<TypeModel>()
+                                             .Concat(Enums)
+                                             .Concat(Interfaces)
+                                             .ToArray();
 }
 
-public class ClassModel
+public abstract class TypeModel
 {
     public required string Name { get; init; }
-    public required List<PropertyModel> Properties { get; init; }
-    public required List<MethodModel> Methods { get; init; }
-    public required List<ConstructorModel> Constructors { get; init; }
+    public required ModifierKind Modifier { get; init; }
+    public required Language Language { get; init; }
+    public List<PropertyModel> Properties { get; init; } = new();
+    public List<MethodModel> Methods { get; init; } = new();
+    public List<ConstructorModel> Constructors { get; init; } = new();
+    public List<EnumValueModel> Values { get; init; } = new();
+    public abstract EnumTypeModel Type { get; }
+
+}
+
+
+public class ClassModel : TypeModel
+{
+    public override EnumTypeModel Type => EnumTypeModel.Class;
+    public override bool Equals(object? obj)
+    {
+        if (obj is ClassModel model)
+        {
+            return this.Name == model.Name;
+        }
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode();
+    }
+
+
+}
+
+public class InterfaceModel : TypeModel
+{
+    public override EnumTypeModel Type => EnumTypeModel.Interface;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is InterfaceModel model)
+        {
+            return this.Name == model.Name;
+        }
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode();
+    }
+}
+
+public class EnumModel : TypeModel
+{
+    public override EnumTypeModel Type => EnumTypeModel.Enum;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is EnumModel other)
+        {
+            return other.Name == Name;
+        }
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode();
+    }
 }
 
 public class ConstructorModel
 {
     public required string Name { get; init; }
+    public required ModifierKind Modifier { get; init; }
     public required List<ParameterModel> Parameters { get; init; }
 
     public override bool Equals(object? obj)
@@ -44,13 +128,11 @@ public class ConstructorModel
     }
 }
 
-
 public class PropertyModel
 {
     public required string Name { get; init; }
     public required string Type { get; init; }
-    public bool IsStatic { get; init; }
-
+    public required ModifierKind Modifier { get; init; }
     public override bool Equals(object? obj)
     {
         if (obj is PropertyModel other)
@@ -69,9 +151,9 @@ public class PropertyModel
 public class MethodModel
 {
     public required string Name { get; init; }
-    public required List<ParameterModel> Parameters { get; init; }
+    public required ModifierKind Modifier { get; init; }
     public required string ReturnType { get; init; }
-    public bool IsStatic { get; init; }
+    public required List<ParameterModel> Parameters { get; init; }
 
     public override bool Equals(object? obj)
     {
@@ -112,47 +194,6 @@ public class ParameterModel
     }
 }
 
-public class EnumModel
-{
-    public required string Name { get; init; }
-    public required List<EnumValueModel> Values { get; init; }
-
-    public override bool Equals(object? obj)
-    {
-        if (obj is EnumModel other)
-        {
-            return other.Name == Name;
-        }
-        return base.Equals(obj);
-    }
-
-    public override int GetHashCode()
-    {
-        return Name.GetHashCode();
-    }
-}
-
-public class InterfaceModel
-{
-    public required string Name { get; init; }
-    public required List<PropertyModel> Properties { get; init; }
-    public required List<MethodModel> Methods { get; init; }
-
-    public override bool Equals(object? obj)
-    {
-        if(obj is InterfaceModel model)
-        {
-            return this.Name == model.Name;
-        }
-        return base.Equals(obj);
-    }
-
-    public override int GetHashCode()
-    {
-        return Name.GetHashCode();
-    }
-}
-
 public class EnumValueModel
 {
     public required string Name { get; init; }
@@ -167,8 +208,33 @@ public class EnumValueModel
         }
         return base.Equals(obj);
     }
+
     public override int GetHashCode()
     {
         return Name.GetHashCode() ^ Value.GetHashCode();
     }
+}
+
+
+public enum ModifierKind
+{
+    Public,
+    Private,
+    Protected,
+    Internal,
+    Static,
+    None
+}
+
+public enum EnumTypeModel
+{
+    Class,
+    Interface,
+    Enum
+}
+
+public enum Language
+{
+    CSharp,
+    TypeScript
 }
