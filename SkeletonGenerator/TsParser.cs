747cef3f689ed7e3c64304145c37d88f41c7e0a9
Rubens Cordeiro <zyoncore@hotmail.com>
2023-08-05 15:15:41 -0300

    updated TsParser

--- SkeletonGenerator/TsParser.cs
+++ SkeletonGenerator/TsParser.cs
@@ -1,10 +1,48 @@
 ﻿using SkeletonGenerator.Domain;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+
+namespace SkeletonGenerator;
 
 class TsParser
 {
     internal static SourceModel Parse(FileInfo fileTs)
     {
-        throw new NotImplementedException();
+        var nodePath = @"C:\Program Files\nodejs\node.exe";
+        var tsParserPath = Path.GetFullPath("../../../../TsParser/dist/main.js");
+        var arguments = $"\"{fileTs.FullName}\"";
+
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = nodePath,
+                Arguments = tsParserPath + " " + arguments,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            }
+        };
+
+        var output = new StringBuilder();
+        process.OutputDataReceived += (sender, args) => output.AppendLine(args.Data);
+        process.Start();
+        process.BeginOutputReadLine();
+        process.WaitForExit();
+
+        var result = output.ToString();
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        var source = JsonSerializer.Deserialize<SourceModel>(result, options);
+        if(source is null)
+        {
+            throw new Exception("Failed to parse TypeScript file");
+        }
+        return source;
     }
 }
 
