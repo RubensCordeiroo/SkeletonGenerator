e0f43d373ec2f7c0cde1386e4d3fe1b97e713c91
Rubens Cordeiro <zyoncore@hotmail.com>
2023-08-05 15:17:05 -0300

    updated Program

--- SkeletonGenerator/Program.cs
+++ SkeletonGenerator/Program.cs
@@ -1,6 +1,8 @@
-﻿using ExtractPublicMethods;
-using SkeletonGenerator.Domain;
+﻿using SkeletonGenerator.Domain;
+using System.IO;
 using System.Reflection.Metadata;
+
+namespace SkeletonGenerator;
 
 class Program
 {
@@ -8,9 +10,9 @@ class Program
     {
         var directoryTS = @"D:\OneDrive\GitHub\SneburBR\Snebur.TS\src\Snebur.TS\src\Utilidade";
         var directoryDotnet = @"D:\OneDrive\GitHub\SneburBR\Snebur.Framework\src\Core\Utilidade";
-        
-        var destTs = @"D:\OneDrive\GitHub\SneburBR\Snebur.TS\src\Snebur.TS\src\Utilidade\generated";
-        var destDotnet = @"D:\OneDrive\GitHub\SneburBR\Snebur.Framework\src\Core\Utilidade\generated";
+
+        var destTs = @"D:\OneDrive\GitHub\SneburBR\snebur-ts\packages\core\src\util\";
+        //var destDotnet = @"D:\OneDrive\GitHub\SneburBR\Snebur.Net\src\Core\src\Util";
 
         if (!Directory.Exists(directoryTS))
         {
@@ -27,7 +29,6 @@ class Program
         }
 
         Console.ForegroundColor = ConsoleColor.White;
-
 
         if (!Directory.Exists(directoryTS))
         {
@@ -55,8 +56,12 @@ class Program
         var allFiles = filesTS.Concat(filesCore).ToArray();
         var groups = allFiles.GroupBy(x => Path.GetFileNameWithoutExtension(x.Name).ToUpper());
 
+        var dictionaryClasse = new Dictionary<string, List<TypeModel>>();
+
         foreach (var group in groups)
         {
+            Console.WriteLine($"Processando: {String.Join(", ", group.Select(x => x.Name))}");
+
             var fileTs = group.FirstOrDefault(x => x.Extension.ToUpper() == ".TS");
             var fileCs = group.FirstOrDefault(x => x.Extension.ToUpper() == ".CS");
 
@@ -66,23 +71,34 @@ class Program
                 SourceCSharp = fileCs != null ? CSharpParser.Parse(fileCs) : null
             };
 
-            //var fileName = Path.GetFileNameWithoutExtension(fileTs.Name);
-            //var dest = Path.Combine(destTs, fileTs.Name);
-            //var skeleton = SkeletonParser.Parse(sources);
-            throw new NotImplementedException();
 
-
+            foreach (var type in sources.SourceTypes)
+            {
+                dictionaryClasse.TryAdd(type.Name, new List<TypeModel>());
+                dictionaryClasse[type.Name].Add(type);
+            }
         }
 
+        var skeletonsTypescript = SkeletonModelFactory.GetSkeletons(dictionaryClasse,
+                                                                    Language.TypeScript,
+                                                                    Language.CSharp);
+
+        foreach (var skeleton in skeletonsTypescript)
+        { 
+            var skeletonString = SkeletonModelParser.Parse(skeleton);
+            var fileDest = Path.Combine(destTs, skeleton.Name + ".skl");
+            File.WriteAllText(fileDest, skeletonString);
+            Console.WriteLine($"Arquivo gerado: {fileDest}");
+        }
     }
 
     public static Dictionary<string, SourcesModel> GetAllPublicsClass(IEnumerable<IGrouping<string, FileInfo>> groups)
     {
         var result = new Dictionary<string, SourcesModel>();
-      
+
         return result;
     }
 
-    
+
 }
 
